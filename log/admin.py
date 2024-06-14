6fd2773963ac4b7ba88730f0093bc4cd7241d1e8
AbrahamRomo7 <rketya@gmail.com>
2024-06-14 03:05:30 -0500

    Backend listo, solo falta implementar el frontend

--- log/admin.py
+++ log/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Equipo,Jugador,Estadio,Goles,Partidos
 # Register your models here.
+admin.site.register(Equipo)
+admin.site.register(Estadio)
+admin.site.register(Goles)
+admin.site.register(Partidos)
+admin.site.register(Jugador)
--- log/models.py
+++ log/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
 
 # Create your models here.
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=150)
+    posicion = models.CharField(max_length=50)
+    equipo = models.ForeignKey(Equipo, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nombre
+
+class Estadio(models.Model):
+    nombre = models.CharField(max_length=50)
+    pais = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Partidos(models.Model):
+    fecha = models.DateField()
+    barcelona_total = models.IntegerField(default=0)
+    madrid_total = models.IntegerField(default=0)
+    ganador = models.CharField(max_length=50,null=True,blank=True)
+    estadio = models.ForeignKey(Estadio, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return str(self.fecha)
+    
+    def determinar_ganador(self):
+        if self.barcelona_total > self.madrid_total:
+            self.ganador = 'Barcelona'
+        elif self.madrid_total > self.barcelona_total:
+            self.ganador = 'Madrid'
+        else:
+            self.ganador = 'Empate'
+
+    def save(self, *args, **kwargs):
+        self.determinar_ganador()
+        super().save(*args, **kwargs)
+
+
+class Goles(models.Model):
+    fecha = models.ForeignKey(Partidos, on_delete=models.CASCADE)
+    jugador = models.ForeignKey(Jugador, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.fecha} - {self.jugador.nombre}"
+    
+@receiver(post_save, sender=Goles)
+def actualizar_puntajes_gol(sender, instance, created, **kwargs):
+    if created:  # Si se crea un nuevo gol
+        partido = instance.fecha
+        equipo_jugador = instance.jugador.equipo
+
+        if equipo_jugador.nombre == 'FC BARCELONA':
+            partido.barcelona_total += 1
+        elif equipo_jugador.nombre == 'REAL MADRID':
+            partido.madrid_total += 1
+        
+        partido.save()
+
+@receiver(post_delete, sender=Goles)
+def actualizar_puntajes_eliminar_gol(sender, instance, **kwargs):
+    partido = instance.fecha
+    equipo_jugador = instance.jugador.equipo
+
+    if equipo_jugador.nombre == 'FC BARCELONA':
+        partido.barcelona_total -= 1
+    elif equipo_jugador.nombre == 'REAL MADRID':
+        partido.madrid_total -= 1
+    
+    partido.save()
+
